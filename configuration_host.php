55624614d11dee7b1a115b9bed2cb05150bfbcb4
S.Karpenko <s.anatolievi4@gmail.com>
2013-02-14 22:10:12 +0200

    Update configuration_host.php

--- configuration_host.php
+++ configuration_host.php
@@ -2,34 +2,5 @@
 class JConfig {
 	public $offline = '0';
 	public $offline_message = 'Сайт закрыт на техническое обслуживание.<br /> Пожалуйста, зайдите позже.';
-	public $display_offline_message = '1';
-	public $offline_image = '';
-	public $sitename = 'Експериментальний творчий центр';
-	public $editor = 'tinymce';
-	public $captcha = '0';
-	public $list_limit = '20';
-	public $access = '1';
-	public $debug = '0';
-	public $debug_lang = '0';
 
-	public $smtpsecure = 'none';
-	public $smtpport = '25';
-	public $caching = '0';
-	public $cache_handler = 'file';
-	public $cachetime = '15';
-	public $MetaDesc = '';
-	public $MetaKeys = '';
-	public $MetaTitle = '1';
-	public $MetaAuthor = '1';
-	public $MetaVersion = '0';
-	public $robots = '';
-	public $sef = '1';
-	public $sef_rewrite = '0';
-	public $sef_suffix = '0';
-	public $unicodeslugs = '0';
-	public $feed_limit = '10';
-	public $log_path = '/home/geekhub1/domains/ecc.ck.ua/public_html/logs';
-	public $tmp_path = '/home/geekhub1/domains/ecc.ck.ua/public_html/tmp';
-	public $lifetime = '15';
-	public $session_handler = 'database';
 }
